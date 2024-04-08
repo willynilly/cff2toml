530f9628b113e78fabd750e31a57af735b84860a
Will Riley <wanderingwill@gmail.com>
2024-04-08 15:07:49 +0200

    Updated CLI. Added metadata for title, description, and repo url.

--- src/cff2toml/cli/app.py
+++ src/cff2toml/cli/app.py
@@ -1,3 +1,4 @@
+from typing import Optional
 import typer
 from typing_extensions import Annotated
 import os
@@ -20,10 +21,10 @@ app.add_typer(app_view, name="view",
               help="View metadata for both CITATION.cff and pyproject.toml files.")
 
 
-@app.command()
-def version():
+@app.command("about")
+def about():
     """
-    Version information about cff2toml
+    Get version and other information about cff2toml
     """
     current_dir: str = os.path.dirname(os.path.abspath(__file__))
     citation_cff_file_path: str = os.path.join(
@@ -58,9 +59,9 @@ def version():
 
 @app_change.command("version")
 def change_version(version: Annotated[str, typer.Argument()],
-                   cff_file_path: Annotated[str, typer.Option(
-                   )] = DEFAULT_CITATION_CFF_FILE_PATH,
-                   pyproject_toml_path: Annotated[str, typer.Option()] = DEFAULT_PYPROJECT_TOML_FILE_PATH):
+                   cff_file_path: Annotated[str, typer.Option(help="CFF file path"
+                                                              )] = DEFAULT_CITATION_CFF_FILE_PATH,
+                   pyproject_toml_path: Annotated[str, typer.Option(help="pyproject.toml file path")] = DEFAULT_PYPROJECT_TOML_FILE_PATH):
     """
     Change both CITATION.CFF and pyproject.toml
     to have the same version.
@@ -81,9 +82,9 @@ def change_version(version: Annotated[str, typer.Argument()],
 
 @app_change.command("license")
 def change_license(license: Annotated[str, typer.Argument()],
-                   cff_file_path: Annotated[str, typer.Option(
-                   )] = DEFAULT_CITATION_CFF_FILE_PATH,
-                   pyproject_toml_path: Annotated[str, typer.Option()] = DEFAULT_PYPROJECT_TOML_FILE_PATH):
+                   cff_file_path: Annotated[str, typer.Option(help="CFF file path"
+                                                              )] = DEFAULT_CITATION_CFF_FILE_PATH,
+                   pyproject_toml_path: Annotated[str, typer.Option(help="pyproject.toml file path")] = DEFAULT_PYPROJECT_TOML_FILE_PATH):
     """
     Change both CITATION.CFF and pyproject.toml
     to have the same license.
@@ -102,11 +103,81 @@ def change_license(license: Annotated[str, typer.Argument()],
         f"[green]changed license for [/green] {pyproject_toml_file.file_path} to [green]{license}[/green]")
 
 
+@app_change.command("title")
+def change_title(title: Annotated[str, typer.Argument()],
+                 cff_file_path: Annotated[str, typer.Option(help="CFF file path"
+                                                            )] = DEFAULT_CITATION_CFF_FILE_PATH,
+                 pyproject_toml_path: Annotated[str, typer.Option(help="pyproject.toml file path")] = DEFAULT_PYPROJECT_TOML_FILE_PATH):
+    """
+    Change both CITATION.CFF and pyproject.toml
+    to have the same title.
+    """
+    cff_file = CffFile(file_path=cff_file_path)
+    pyproject_toml_file = PyprojectTomlFile(file_path=pyproject_toml_path)
+
+    file_sync = CffAndPyprojectTomlFileSynchronizer(
+        cff_file=cff_file, pyproject_toml_file=pyproject_toml_file)
+    file_sync.set_title(title=title)
+    file_sync.save()
+    print(f"[yellow]changing title metadata...[/yellow]")
+    print(
+        f"[green]changed title for[/green] {cff_file.file_path} to [green]{title}[/green]")
+    print(
+        f"[green]changed title for [/green] {pyproject_toml_file.file_path} to [green]{title}[/green]")
+
+
+@app_change.command("description")
+def change_description(description: Annotated[str, typer.Argument()],
+                       cff_file_path: Annotated[str, typer.Option(help="CFF file path"
+                                                                  )] = DEFAULT_CITATION_CFF_FILE_PATH,
+                       pyproject_toml_path: Annotated[str, typer.Option(help="pyproject.toml file path")] = DEFAULT_PYPROJECT_TOML_FILE_PATH):
+    """
+    Change both CITATION.CFF and pyproject.toml
+    to have the same description.
+    """
+    cff_file = CffFile(file_path=cff_file_path)
+    pyproject_toml_file = PyprojectTomlFile(file_path=pyproject_toml_path)
+
+    file_sync = CffAndPyprojectTomlFileSynchronizer(
+        cff_file=cff_file, pyproject_toml_file=pyproject_toml_file)
+    file_sync.set_description(description=description)
+    file_sync.save()
+    print(f"[yellow]changing description metadata...[/yellow]")
+    print(
+        f"[green]changed description for[/green] {cff_file.file_path} to [green]{description}[/green]")
+    print(
+        f"[green]changed description for [/green] {pyproject_toml_file.file_path} to [green]{description}[/green]")
+
+
+@app_change.command("repo")
+def change_code_repository_url(code_repository_url: Annotated[str, typer.Argument()],
+                               cff_file_path: Annotated[str, typer.Option(help="CFF file path"
+                                                                          )] = DEFAULT_CITATION_CFF_FILE_PATH,
+                               pyproject_toml_path: Annotated[str, typer.Option(help="pyproject.toml file path")] = DEFAULT_PYPROJECT_TOML_FILE_PATH):
+    """
+    Change both CITATION.CFF and pyproject.toml
+    to have the same code repository url.
+    """
+    cff_file = CffFile(file_path=cff_file_path)
+    pyproject_toml_file = PyprojectTomlFile(file_path=pyproject_toml_path)
+
+    file_sync = CffAndPyprojectTomlFileSynchronizer(
+        cff_file=cff_file, pyproject_toml_file=pyproject_toml_file)
+    file_sync.set_code_repository_url(
+        code_repository_url=code_repository_url)
+    file_sync.save()
+    print(f"[yellow]changing code_repository_url metadata...[/yellow]")
+    print(
+        f"[green]changed code_repository_url for[/green] {cff_file.file_path} to [green]{code_repository_url}[/green]")
+    print(
+        f"[green]changed code_repository_url for [/green] {pyproject_toml_file.file_path} to [green]{code_repository_url}[/green]")
+
+
 @app_view.command("version")
 def view_version(
-        cff_file_path: Annotated[str, typer.Option(
-        )] = DEFAULT_CITATION_CFF_FILE_PATH,
-        pyproject_toml_path: Annotated[str, typer.Option()] = DEFAULT_PYPROJECT_TOML_FILE_PATH):
+        cff_file_path: Annotated[str, typer.Option(help="CFF file path"
+                                                   )] = DEFAULT_CITATION_CFF_FILE_PATH,
+        pyproject_toml_path: Annotated[str, typer.Option(help="pyproject.toml file path")] = DEFAULT_PYPROJECT_TOML_FILE_PATH):
     """
     View version information for CITATION.CFF and pyproject.toml
     """
@@ -122,9 +193,9 @@ def view_version(
 
 @app_view.command("license")
 def view_license(
-        cff_file_path: Annotated[str, typer.Option(
-        )] = DEFAULT_CITATION_CFF_FILE_PATH,
-        pyproject_toml_path: Annotated[str, typer.Option()] = DEFAULT_PYPROJECT_TOML_FILE_PATH):
+        cff_file_path: Annotated[str, typer.Option(help="CFF file path"
+                                                   )] = DEFAULT_CITATION_CFF_FILE_PATH,
+        pyproject_toml_path: Annotated[str, typer.Option(help="pyproject.toml file path")] = DEFAULT_PYPROJECT_TOML_FILE_PATH):
     """
     View license information for CITATION.CFF and pyproject.toml
     """
@@ -136,3 +207,57 @@ def view_license(
         f"{cff_file.file_path} has license [green]{cff_file.metadata_license}[/green]")
     print(
         f"{pyproject_toml_file.file_path} has license [green]{pyproject_toml_file.metadata_project_license}[/green]")
+
+
+@app_view.command("title")
+def view_title(
+        cff_file_path: Annotated[str, typer.Option(help="CFF file path"
+                                                   )] = DEFAULT_CITATION_CFF_FILE_PATH,
+        pyproject_toml_path: Annotated[str, typer.Option(help="pyproject.toml file path")] = DEFAULT_PYPROJECT_TOML_FILE_PATH):
+    """
+    View title information for CITATION.CFF and pyproject.toml
+    """
+    cff_file = CffFile(file_path=cff_file_path)
+    pyproject_toml_file = PyprojectTomlFile(file_path=pyproject_toml_path)
+
+    print(f"[yellow]viewing title metadata...[/yellow]")
+    print(
+        f"{cff_file.file_path} has title [green]{cff_file.metadata_title}[/green]")
+    print(
+        f"{pyproject_toml_file.file_path} has title [green]{pyproject_toml_file.metadata_project_name}[/green]")
+
+
+@app_view.command("repo")
+def view_code_repository_url(
+        cff_file_path: Annotated[str, typer.Option(help="CFF file path"
+                                                   )] = DEFAULT_CITATION_CFF_FILE_PATH,
+        pyproject_toml_path: Annotated[str, typer.Option(help="pyproject.toml file path")] = DEFAULT_PYPROJECT_TOML_FILE_PATH):
+    """
+    View code repository url information for CITATION.CFF and pyproject.toml
+    """
+    cff_file = CffFile(file_path=cff_file_path)
+    pyproject_toml_file = PyprojectTomlFile(file_path=pyproject_toml_path)
+
+    print(f"[yellow]viewing code_repository_url metadata...[/yellow]")
+    print(
+        f"{cff_file.file_path} has code repository url [green]{cff_file.metadata_repository_code}[/green]")
+    print(
+        f"{pyproject_toml_file.file_path} has code repository url [green]{pyproject_toml_file.metadata_project_urls_source}[/green]")
+
+
+@app_view.command("description")
+def view_description(
+        cff_file_path: Annotated[str, typer.Option(help="CFF file path"
+                                                   )] = DEFAULT_CITATION_CFF_FILE_PATH,
+        pyproject_toml_path: Annotated[str, typer.Option(help="pyproject.toml file path")] = DEFAULT_PYPROJECT_TOML_FILE_PATH):
+    """
+    View description information for CITATION.CFF and pyproject.toml
+    """
+    cff_file = CffFile(file_path=cff_file_path)
+    pyproject_toml_file = PyprojectTomlFile(file_path=pyproject_toml_path)
+
+    print(f"[yellow]viewing description metadata...[/yellow]")
+    print(
+        f"{cff_file.file_path} has description [green]{cff_file.metadata_abstract}[/green]")
+    print(
+        f"{pyproject_toml_file.file_path} has description [green]{pyproject_toml_file.metadata_project_description}[/green]")
